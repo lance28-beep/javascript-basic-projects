b17bbcb378533c8cc255d2ff81ba6abfb7939388
Lance Valle <vallelance8@gmail.com>
2022-11-18 21:01:34 +0800

    add rgba js code random

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,24 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function () {
+  // get random numbers between 0 - 3 colors[3]
+  // const firstNum = Math.floor(Math.random() * 254)
+  // const secondNum = Math.floor(Math.random() * 254)
+  // const thirdNum = Math.floor(Math.random() * 254)
+  // const rgba = `rgba(${firstNum},${secondNum},${thirdNum})`
+  const rgba = randomNumbers()
+  document.body.style.backgroundColor = rgba
+  color.textContent = rgba
+})
+
+//random numbers
+const randomNumbers = () => {
+  const arr = []
+
+  for (let i = 4; i >= arr.length; i--) {
+    arr.push(Math.floor(Math.random() * 254))
+  }
+  console.log(arr)
+  return `rgba(${arr[0]},${arr[1]},${arr[2]})`
+}
