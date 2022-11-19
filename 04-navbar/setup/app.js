50ed9e47a36709b77cc2f69c137710e00d73c775
Lance Valle <vallelance8@gmail.com>
2022-11-19 15:19:43 +0800

    add toggle

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,15 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const toggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+toggle.addEventListener('click', function () {
+  // if (links.classList.contains('show-links')) {
+  //   links.classList.remove('show-links')
+  // } else {
+  //   links.classList.add('show-links')
+  // }
+  links.classList.toggle('show-links')
+})
